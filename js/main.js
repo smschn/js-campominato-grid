a717084e21611629737725edc4a1792c862202a8
Luca Somaschini <somas99@gmail.com>
2022-07-13 15:47:44 +0200

    Added function to create squares

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 const square_containerDom = document.getElementById('square_container');
-console.log(square_containerDom); // debug
 
 for (let i=1; i<101; i++) {
-    // creo nuovo elemento <div> ad ogni ciclo
-    const newSquare = document.createElement('div');
-    // aggiungo classe .square all'elemento
-    newSquare.classList.add('square');
-    // metto ('appendo') newSquare dentro square_containerDom
-    square_containerDom.append(newSquare);
+    const newSquare = createSquare();
+}
+
+// funzione per creare <div square>
+function createSquare() {
+        // creo nuovo elemento <div> ad ogni ciclo
+        const newSquare = document.createElement('div');
+        // aggiungo classe .square all'elemento
+        newSquare.classList.add('square');
+        // metto ('appendo') newSquare dentro square_containerDom
+        square_containerDom.append(newSquare);
+        return newSquare;
 }
