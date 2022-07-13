052311db5f7cf6911e05025eb6333a32e820f371
Luca Somaschini <somas99@gmail.com>
2022-07-13 16:30:55 +0200

    Bonus: added folders and files

---
+++ bonus/js/main.js
@@ -0,0 +1,39 @@
+// richiamare il container
+const square_containerDom = document.getElementById('square_container');
+
+// richiamare il button
+const buttonDom = document.getElementById('play_btn');
+
+// aggiungere evento di attivazione del ciclo
+buttonDom.addEventListener('click',
+function() {
+    for (let i=1; i<101; i++) {
+        // funzione per creare uno square ad ogni ciclo da 1 a 100
+        const newSquare = createSquare();
+        // stampo il numero su ogni quadrato
+        newSquare.append(i);
+    }
+})
+
+// funzione per creare <div square>
+function createSquare() {
+        // creo nuovo elemento <div> ad ogni ciclo
+        const newSquare = document.createElement('div');
+        // aggiungo classe .square all'elemento
+        newSquare.classList.add('square');
+        // metto ('appendo') newSquare dentro square_containerDom
+        square_containerDom.append(newSquare);
+        // creare un event listener su ogni singola cella che cambi sfondo allo square e scriva in console il numero della cella
+        newSquare.addEventListener('click', 
+        function(){
+            // se non ho ancora cliccato, imposto lo sfondo azzurrino; altrimenti, se ho già cliccato e quindi ho già lo sfondo azzurrino, reimposto lo sfondo verde
+            if (this.style.backgroundColor == 'lightblue') {
+                this.style.backgroundColor = '#7fffd4';
+            } else {
+                this.style.backgroundColor = 'lightblue';
+            }
+            console.log(`The number of the square is: ${this.textContent}`);
+        })
+        // ritorno tutto quanto fatto su square
+        return newSquare;
+}
